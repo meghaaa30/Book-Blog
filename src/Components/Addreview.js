88fb76ae54616035a1fd004efeabd76176ee27e0
Tanishka <tanishkasingh2004@gmail.com>
2024-05-09 01:06:35 +0530

    Add text to the first page

--- src/Components/Addreview.js
+++ src/Components/Addreview.js
@@ -1,29 +1,28 @@
 import React from "react";
 
-function Addreview() {
-    return (
-        
-       <div className="addreview">
-    <form >
-  <div className="form-group">
-    <label htmlFor="Book" className="font">Name of the book: </label>
-    <input type="text" className="form-control" id="book_name" placeholder="Book's Name"/>
-  </div>
-  <div className="form-group">
-    <label htmlFor="Author" className="font">Author's Name: </label>
-    <input type="text" className="form-control" id="author" placeholder="Author Name"/>
-  </div>
-  <div className="form-group">
-  <label htmlFor="review" className="font">Add a review: </label>
-    <input type="textarea" className="form-control texting" id="review"/>
-    
-  </div>
-  <div className="btn">
-  <button type="submit" className="btn btn-primary">Add Review</button>
-  </div>
-</form>
-</div>
+function AddReview() {
+  return (
+    <div className="addreview">
+      <form >
+        <div className="form-group">
+          <label htmlFor="Book" className="font">Name of the book: </label>
+          <input type="text" className="form-control" id="book_name" placeholder="Book's Name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="Author" className="font">Author's Name: </label>
+          <input type="text" className="form-control" id="author" placeholder="Author's Name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="review" className="font">Add a review: </label>
+          <input type="textarea" className="form-control texting" id="review" />
 
-)
+        </div>
+        <div className="btn">
+          <button type="submit" className="btn btn-primary">Add Review</button>
+        </div>
+      </form>
+    </div>
+  );
 }
-export default Addreview;
+
+export default AddReview;
--- src/Components/BookStrip.js
+++ src/Components/BookStrip.js
@@ -37,21 +37,36 @@ const BookStrip = () => {
     }, [allBooks, displayedBooks]);
 
     return (
-        <div style={{ display: 'grid', gridTemplateColumns: '130px 130px 130px', gridTemplateRows: '180px 180px 180px', gap: '10px', justifyContent: "right", marginTop: "115px", marginRight: "10px" }}>
-            {displayedBooks.map((book, index) => (
-                <div
-                    key={book.key}
-                    className="book"
-                    style={{
-                        opacity: index === currentIndex ? 0 : 1, // Fade out/in based on index
-                        transition: 'opacity 1s ease-in-out', // CSS transition for fading effect
-                    }}
-                >
-                    <a href={`https://openlibrary.org${book.key}`} target="_blank" rel="noopener noreferrer">
-                        <img src={`https://covers.openlibrary.org/b/id/${book.cover_id}-M.jpg`} alt={book.title} style={{ width: '130px', height: '180px' }} />
-                    </a>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div style={{ display: 'flex', alignItems: 'flex-start', marginRight: '430px', marginLeft: '40px' }}>
+                <div>
+                    <h1 style={{ fontFamily: 'Poppins', fontSize: '4.5rem', fontWeight: '700', color: 'white', textAlign: 'left', marginBottom: '0', paddingBottom: '12px' }}>
+                        Book-Blog
+                    </h1>
+                    <p style={{ fontFamily: 'Poppins', fontSize: '2.5rem', fontWeight: '500', color: 'white', textAlign: 'left', marginBottom: '0', marginTop: '0' }}>
+                        Discover your next favorite book with
+                    </p>
+                    <p style={{ fontFamily: 'Poppins', fontSize: '2.5rem', fontWeight: '500', color: 'white', textAlign: 'left', marginTop: '0' }}>
+                        our insightful reviews.
+                    </p>
                 </div>
-            ))}
+            </div>
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px', alignItems: 'flex-start', justifyContent: 'flex-end' }}>
+                {displayedBooks.map((book, index) => (
+                    <div
+                        key={book.key}
+                        className="book"
+                        style={{
+                            opacity: index === currentIndex ? 0 : 1, // Fade out/in based on index
+                            transition: 'opacity 1s ease-in-out', // CSS transition for fading effect
+                        }}
+                    >
+                        <a href={`https://openlibrary.org${book.key}`} target="_blank" rel="noopener noreferrer">
+                            <img src={`https://covers.openlibrary.org/b/id/${book.cover_id}-M.jpg`} alt={book.title} style={{ width: '130px', height: '180px' }} />
+                        </a>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
