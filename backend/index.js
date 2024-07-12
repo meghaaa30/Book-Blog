b1c9cfa78e41394c92ea70ba0cdb778e743526f3
Tanishka <tanishkasingh2004@gmail.com>
2024-07-12 22:53:30 +0530

    Fix Bugs

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const connectTo = require('./db');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 // Middleware
 app.use(cors());
